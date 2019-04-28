f4cd605090d93b8ab43a4558a99fdec0c27eb784
Mizuho Isogai <isogai@isogaimizuhonoMacBook-puro.local>
2019-04-28 16:09:20 +0900

    コントローラー、jbuilderの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   private
 
   def user_params
