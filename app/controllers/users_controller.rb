d07473369b4f01a29a7b44202301d5c86e46d414
Mizuho Isogai <isogai@isogaimizuhonoMacBook-puro.local>
2019-04-29 09:56:58 +0900

    自ログインユーザー排除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+    @users = User.where('name LIKE(?) and id !=?' , "%#{params[:name]}%", current_user)
     respond_to do |format|
       format.html
       format.json
