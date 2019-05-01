f0fc83bfcd5fa7ba5f41e6c6f75eb33003a06607
Mizuho Isogai <isogai@isogaimizuhonoMacBook-puro.local>
2019-05-01 15:44:44 +0900

    アクション追加

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,6 @@
 class Api::MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @messages = @group.message.where('id > ?', params[:id])
   end
 end
